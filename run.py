509a1e312fcf802ab05c30cdd770f798fa280b2b
Patrick Aselin <26674234+pjaselin@users.noreply.github.com>
2022-01-03 23:52:09 -0500

    eod

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
-import time
 import multiprocessing as mp
 
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
-from luma.core.virtual import viewport, sevensegment
+from luma.core.virtual import sevensegment
 
 from .enum import TrackEvent
 
@@ -17,11 +16,23 @@ seg = sevensegment(device)
 # - when IR sensor closes, stop the loop and report the time back to the main event loop
 # - report each time and the position to the display
 
-seg.text = "--------"
-time.sleep(3)
+def display_to_led(message_dict):
+    """"function to take the dictionary of information we want to show and convert it to 
+    a single string for the LED array"""
+    text_to_display = ""
+    for track in message_dict:
+        text_to_display += text_to_display[track]["position"]
+        text_to_display += text_to_display[track]["time"]
+    seg.text = text_to_display
 
+# set the defaul/min car position
 car_position = 1
 
+# set the default LED messages
+display_messages = {track: dict(position="-", time="----") for track in ["track1", "track2"]}
+display_to_led(display_messages)
+
+# set the main event queue
 main_queue = mp.Queue()
 
 while True:
